470a5a4adf23e84ed255723487ecb46d77fd719e
HA\lmeza <lmeza@highlandag.com>
2022-08-25 15:58:05 -0500

    adding antd and some basic table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HH';
+  dataSet = [
+    {
+      name: 'Jamie',
+      age: 'nunya',
+      address: 'why?'
+    },
+    {
+      name: 'Josh',
+      age: '60?',
+      address: 'Somewhere in NC'
+    },
+    {
+      name: 'Luis',
+      age: '34',
+      address: 'Humid Houston'
+    }
+  ]
+  alerting() {
+    alert('welcome to our POC');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzButtonModule,
+    NzTableModule,
+    NzDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
